fbd0a5bf6b91a0fa12ba43e6a0ce00532d14714e
albatraozrus <newtonist@yandex.ru>
2021-04-11 03:44:50 +0300

    Cosmetic changes

--- shell.cpp
+++ shell.cpp
@@ -2,31 +2,6 @@
 
 #include <iostream>
 
-// namespace {
-//     void ReadFromInput(char *pathToFile, std::string &inputText, bool isFile)
-//     {
-//         if (isFile)
-//         {
-//             std::ifstream inputFile(pathToFile, std::ios_base::in | std::ios_base::binary);
-
-//             if (!inputFile)
-//             {
-//                 std::cerr << "Error: Incorrect path to input file!\n";
-//                 exit(EXIT_FAILURE);
-//             }
-
-//             while (inputFile)
-//                 inputFile >> inputText;
-
-//             inputFile.close();
-//         }
-//         else
-//             while (getline(std::cin, inputText, '\0'))
-//             {
-//             }
-//     }
-// }
-
 int main(int argc, char **argv)
 {
     if (argc == 1) {
@@ -34,9 +9,14 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
+    try {
     Translator translator(argv[1]);
     translator.Translate();
     translator.Dump();
+    }
+    catch(std::runtime_error& exception){
+        std::cerr << exception.what();
+    }
 
     return 0;
 }
